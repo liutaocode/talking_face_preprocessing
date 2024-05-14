c687e8d914d297ed3a5fa8677e150945c9849b8e
Tao Liu <liutaw@sjtu.edu.cn>
2024-05-14 08:36:46 +0000

    Refine the code and enhance its robustness

--- extract_cropped_faces.py
+++ extract_cropped_faces.py
@@ -6,19 +6,18 @@ from torchlm.tools import faceboxesv2
 from torchlm.models import pipnet
 from tqdm import tqdm
 import os
-import numpy as np
 
-def main(from_dir_prefix, output_dir_prefix, expanded_ratio):
-    
+def main(from_dir_prefix, output_dir_prefix, expanded_ratio, skip_per_frame):
     os.makedirs(output_dir_prefix, exist_ok=True)
     
     device = torch.device("cuda:0")
     torchlm.runtime.bind(faceboxesv2(device=device))
-
     torchlm.runtime.bind(
-        pipnet(backbone="resnet18", pretrained=True,
-               num_nb=10, num_lms=68, net_stride=32, input_size=256,
-               meanface_type="300w", map_location=device, checkpoint=None)
+        pipnet(
+            backbone="resnet18", pretrained=True, num_nb=10, num_lms=68, 
+            net_stride=32, input_size=256, meanface_type="300w", 
+            map_location=device, checkpoint=None
+        )
     )
 
     for mp4_name in tqdm(os.listdir(from_dir_prefix)):
@@ -31,53 +30,68 @@ def main(from_dir_prefix, output_dir_prefix, expanded_ratio):
         video = cv2.VideoCapture(from_mp4_file_path)
         index = 0
         bboxes_lists = []
+        width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        
         while video.isOpened():
             success = video.grab()
-
-            if index % 25 == 0: # skip every one seconds
+            if index % skip_per_frame == 0:
                 success, frame = video.retrieve()
                 if not success:
                     break
                 landmarks, bboxes = torchlm.runtime.forward(frame)
-
+                    
                 if bboxes.shape == (1, 5):
-                    bboxes_lists.append(bboxes)
+                    bboxes_lists.append(bboxes[0])
+                elif bboxes.shape[0] > 0:
+                    # If multiple persons exist, select the one with the largest width
+                    max_bboxes = max(bboxes, key=lambda bbox: bbox[2] - bbox[0])
+                    bboxes_lists.append(max_bboxes)
             index += 1
 
         x_center_lists, y_center_lists, width_lists, height_lists = [], [], [], []
-        for i in range(len(bboxes_lists)):
-            x1, y1, x2, y2 = bboxes_lists[i][0, :4]
-
+        for bbox in bboxes_lists:
+            x1, y1, x2, y2 = bbox[:4]
             x_center, y_center = (x1 + x2) / 2, (y1 + y2) / 2
             x_center_lists.append(x_center)
             y_center_lists.append(y_center)
             width_lists.append(x2 - x1)
             height_lists.append(y2 - y1)
             
-        if not x_center_lists or not y_center_lists or not width_lists or not height_lists:
+        if not (x_center_lists and y_center_lists and width_lists and height_lists):
             print(f"Face may not exist, please check the video: {mp4_name}")
+            exit(0)
             continue
 
-        x_center_lists.sort(), y_center_lists.sort(), width_lists.sort(), height_lists.sort()
-        x_center = x_center_lists[int(len(x_center_lists) / 2)]
-        y_center = y_center_lists[int(len(y_center_lists) / 2)]
+        x_center = sorted(x_center_lists)[len(x_center_lists) // 2]
+        y_center = sorted(y_center_lists)[len(y_center_lists) // 2]
+        median_width = sorted(width_lists)[len(width_lists) // 2]
+        median_height = sorted(height_lists)[len(height_lists) // 2]
+        
+        expanded_width = int(median_width * (1 + expanded_ratio))
+        expanded_height = int(median_height * (1 + expanded_ratio))
+        
+        fixed_cropped_width = min(max(expanded_width, expanded_height), width, height)
+        
+        x1, y1 = int(x_center - fixed_cropped_width / 2), int(y_center - fixed_cropped_width / 2)
 
-        new_width, new_height = width_lists[int(len(width_lists) / 2)], height_lists[int(len(height_lists) / 2)]
-        new_width, new_height = int(new_width * (1 + expanded_ratio)), int(new_height * (1 + expanded_ratio))
-        max_len = max(new_width, new_height)
-        fixed_cropped_width = max_len
-
-        x1, y1 = int(x_center - max_len / 2), int(y_center - max_len / 2)
-
-        if x1 < 0:
-            x1 = 0
-
-        if y1 < 0:
-            y1 = 0
-
-        cmd = 'ffmpeg -i %s -filter:v "crop=%d:%d:%d:%d,pad=%d:%d:%d:%d" -c:a copy %s -y' % (
-            from_mp4_file_path, fixed_cropped_width, max_len, x1, y1, fixed_cropped_width, fixed_cropped_width, x1 + fixed_cropped_width, y1 + fixed_cropped_width, to_mp4_file_path)
-        os.system(cmd)
+        if args.debug:
+            print(cmd)
+            cmd = (
+                f'ffmpeg -i {from_mp4_file_path} -filter:v "crop={fixed_cropped_width}:{fixed_cropped_width}:{x1}:{y1},'
+                f'pad={fixed_cropped_width}:{fixed_cropped_width}:{x1 + fixed_cropped_width}:{y1 + fixed_cropped_width}" '
+                f'-c:a copy {to_mp4_file_path} -y'
+            )
+        else:
+            cmd = (
+                f'ffmpeg -i {from_mp4_file_path} -filter:v "crop={fixed_cropped_width}:{fixed_cropped_width}:{x1}:{y1},'
+                f'pad={fixed_cropped_width}:{fixed_cropped_width}:{x1 + fixed_cropped_width}:{y1 + fixed_cropped_width}" '
+                f'-c:a copy {to_mp4_file_path} -y -loglevel quiet'
+            )
+        
+        if os.system(cmd) != 0 and args.debug:
+            print(f"Error executing command: {cmd}, please check")
+            break
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process some integers.')
@@ -87,7 +101,10 @@ if __name__ == "__main__":
                         help='output directory where cropped faces will be stored')
     parser.add_argument('--expanded_ratio', type=float, default=0.6,
                         help='ratio to expand the bounding box for cropping')
+    parser.add_argument('--skip_per_frame', type=int, default=25,
+                        help='number of frames to skip during processing')
+    parser.add_argument('--debug', action='store_true', help='Enable debug mode')
+
     args = parser.parse_args()
 
-    main(args.from_dir_prefix, args.output_dir_prefix, args.expanded_ratio)
-
+    main(args.from_dir_prefix, args.output_dir_prefix, args.expanded_ratio, args.skip_per_frame)
